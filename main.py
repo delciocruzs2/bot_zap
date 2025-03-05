6cb469a7319a6a3fc532ff51e8b52e617349c4bc
delciocruz <delciocruzoficial@gmail.com>
2025-03-05 14:26:24 -0300

    test(deploy): teste de api

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-""" O desenvolvimento do bot está em fase inicial"""
+from flask import Flask, request, json, jsonify
 
-print('primeiro commit de teste')
+bot = Flask(__name__)
+    
+# Endpoint homepage   
+@bot.route('/')
+def homepage() -> str:
+    """ URL primary"""
+    return 'Bem vindo a pagina inicial do bot'
+
+# Endpoint webhook
+@bot.route('/webhook', methods=['POST'])
+def handle_webhook() -> json:
+    try:
+        information = request.get_json()
+        return jsonify(f'content: {information}',200)
+    except ValueError as ve:
+        return jsonify(f'Json Error: {ve}',404)
+
+if __name__=='__main__':
+    bot.run(debug=True)
